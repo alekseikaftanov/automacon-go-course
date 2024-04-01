c2b13c31406a9b8b21c020fa75b795610b33be63
alekseikaftanov <aleksei.kaftanov@gmail.com>
2024-04-01 15:06:51 +0300

    Hi!

---
+++ 002/main2.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+
+	var a rune
+	var b byte
+
+	println(a, b)
+
+}
---
+++ 003/main.go
@@ -0,0 +1,62 @@
+package main
+
+// Задача 3.1. Необходимо создать глобальную константу и вывести её значение в консоль
+
+// const myConst = 10
+
+// func main() {
+// 	fmt.Println(myConst)
+// }
+
+// Задача 3.2. Необходимо создать локальную константу и вывести её значение в консоль
+
+// func main() {
+// 	const myConst = 10
+// 	fmt.Println(myConst)
+// }
+
+// Задача 3.3. Необходимо создать ukj,fkmye. константу и затенить ее значение
+
+// const myConst = 10
+
+// func main() {
+// 	const myConst = 20
+// 	fmt.Println(myConst)
+// }
+
+// Задача 3.4. Создание группы глобальных констант
+
+// const (
+// 	myConst1 = 1
+// 	myConst2 = 2
+// 	myConst3 = 3
+// 	myConst4 = 4
+// 	myConst5 = 5
+// )
+
+// func main() {
+// 	fmt.Println(myConst1, myConst2, myConst3, myConst4, myConst5)
+// }
+
+// Задача 3.5. Создание группы локальных констант
+
+// func main() {
+// 	const (
+// 		myConst1 = 1
+// 		myConst2 = 2
+// 		myConst3 = 3
+// 		myConst4 = 4
+// 		myConst5 = 5
+// 	)
+// 	fmt.Println(myConst1, myConst2, myConst3, myConst4, myConst5)
+// }
+
+// Задача 3.6. Приведение типизированной константы к значению float
+
+// func main() {
+// 	const n int = 5
+// 	m := 3.4 + float64(n)
+// 	fmt.Println(m)
+// }
+
+// Задача 3.7. Генератор Iota
