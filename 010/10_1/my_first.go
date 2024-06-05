6408a588af484d62627d1353b5ce8aa548b79731
alekseikaftanov <aleksei.kaftanov@gmail.com>
2024-06-05 17:22:33 +0300

    010_1/10_3

--- 010/10_1/my_first.go
+++ 010/10_1/my_first.go
@@ -1,7 +1,13 @@
 package first
 
+import "fmt"
+
 const name = "Hello, Dolly!"
 
 func Hello() string {
 	return name
+}
+
+func main() {
+	fmt.Println(Hello())
 }
---
+++ 010/10_3/second.go
@@ -0,0 +1,11 @@
+package second
+
+import "fmt"
+
+func Hello() string {
+	return "Hello, Louis!"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
